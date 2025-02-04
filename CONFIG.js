0e54be76e3a4febf3a318ef5a833e82b1c015658
Hector Martinez <hectormmgdth@gmail.com>
2025-02-03 19:44:23 -0600

    cambios

--- CONFIG.js
+++ CONFIG.js
@@ -1,10 +1,10 @@
 const CONFIGDATA = {
   titleHeader: "Hola, tengo algo que decir. </br> ¡Sé mi cita de San Valentin!",
   descriptionHeader:
-  "Por favor responde honestamente. Si permaneces en silencio y te vas, significa que ESTÁS DE ACUERDO. (Y te secuestrare el 10 de febrero)",
+  "Por favor responde honestamente. Si permaneces en silencio y te vas, significa que ESTÁS DE ACUERDO.",
   buttonYes: "Sí acepto",
   buttonNo: "Nel no quiero",
   titleModar: "Gracias.",
   descriptionModar:
-  "Si ves esto significa que aceptaste, ya que el 14 de Febrero es miercoles, nuestra cita tendrá como fecha el 10 de Febrero. </br> ¡Gracias por darle una oportunidad a mi encanto peculiar! Estoy seguro de que será un día maravilloso ¡Prepárate para risas y buena compañía! cerrando con el esperando concierto de José Madero ¡Gracias por decir sí! </br> Te quiero 💕",
+  "Si ves esto significa que aceptaste</br> ¡Gracias por darle una oportunidad a mi encanto peculiar! Estoy seguro de que No te arrepentiras</br> Te quiero Bris💕",
 };
